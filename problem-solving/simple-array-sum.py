982131404895b259b1c8c5b1991f5c81adbdc396
paulzay <paulotieno2@gmail.com>
2020-05-24 19:38:19 +0300

    simple sum sltn

---
+++ problem-solving/simple-array-sum.py
@@ -0,0 +1,10 @@
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+    total = 0
+    for i in ar:
+        total += i
+    return total
