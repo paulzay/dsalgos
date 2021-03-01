0895f144776546ef2c554d52f4c7d005a9034e12
paulzay <paulotieno2@gmail.com>
2021-03-01 22:51:05 +0300

    complete same tree

---
+++ leetcode/100_Same_Tree.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
+def is_same_tree(p, q)
+  return p == q if p.nil? || q.nil?
+  return false if p.val != q.val
+
+  is_same_tree(p.right, q.right) && is_same_tree(p.left, q.left)
+end
