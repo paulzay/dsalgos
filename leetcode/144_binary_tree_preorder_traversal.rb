376e5ff349967782ed17168c561c9b5f86195da4
paulzay <paulotieno2@gmail.com>
2021-03-05 15:03:23 +0300

    challenge complete

---
+++ leetcode/144_binary_tree_preorder_traversal.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+def preorder_traversal(root)
+  return [] if root.nil?
+
+  [root.val] + preorder_traversal(root.left) + preorder_traversal(root.right)
+end
